74110c54c43ea19a29fca2c8d80996847f96d5fd
Karen Lei <karenwinnielei@gmail.com>
2020-06-11 15:25:56 -0700

    createStore and Provider

--- project/src/index.js
+++ project/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import thunk from 'redux-thunk';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 serviceWorker.unregister();
