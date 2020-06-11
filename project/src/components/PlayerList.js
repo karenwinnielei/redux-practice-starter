13fc16b2623b3cee9d4b13fa788f2fd7e55f553b
Karen Lei <karenwinnielei@gmail.com>
2020-06-11 15:48:03 -0700

    add action and connect

--- project/src/components/PlayerList.js
+++ project/src/components/PlayerList.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PlayerCard from './PlayerCard';
 
+import { connect } from 'react-redux';
+import { fetchPlayers } from '../actions';
+
 const PlayerList = (props) => {
+  console.log(props);
+
+  useEffect(() => {
+    props.fetchPlayers();
+  }, [props]);
+
   return (
     <div>
-      <p>PlayerList</p>
-      <PlayerCard />
+      <p>Player List</p>
+      <div className='card-container'>
+        {props.player.map((player) => (
+          <PlayerCard key={player.id} player={player} />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default PlayerList;
+const mapStateToProps = (state) => {
+  return {
+    // left props: right state in store
+    players: state.players,
+    isFetching: state.isFetching,
+    error: state.error,
+  };
+};
+
+export default connect(mapStateToProps, { fetchPlayers })(PlayerList);
