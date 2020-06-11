d39b3754a7106c002d378ac09c0229bac8337573
Karen Lei <karenwinnielei@gmail.com>
2020-06-11 16:18:50 -0700

    build player form

--- project/src/components/PlayerList.js
+++ project/src/components/PlayerList.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import PlayerCard from './PlayerCard';
 
@@ -9,7 +10,7 @@ const PlayerList = (props) => {
 
   useEffect(() => {
     props.fetchPlayers();
-  }, [props]);
+  }, []);
 
   return (
     <div>
